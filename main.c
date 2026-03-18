938e66249800a87894a3d14f8dfbf76704da4c9f
Rares-AndreiVasiu <rares_vasiu03@yahoo.ro>
2026-03-18 08:26:04 +0200

    overclocked core to 250MHZ

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "pico/stdlib.h"
+#include "hardware/clocks.h"
+
+#define PICO_OVERCLOCK_HZ 250000
+#define GPIO_PIN_PWM 4
+
 
 int main()
 {
+  set_sys_clock_khz(PICO_OVERCLOCK_HZ, true);
+   
   stdio_init_all();
   
   while(1);
