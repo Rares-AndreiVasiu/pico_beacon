900caa7da90a27ce435963dcb1f5d241641939ea
Rares-AndreiVasiu <rares_vasiu03@yahoo.ro>
2026-03-18 12:37:08 +0200

    configure pwm on gp4 with 50mhz carrier wave

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 #include <string.h>
 
 #include "pico/stdlib.h"
+#include "pico/multicore.h""
 #include "hardware/clocks.h"
 
 #define PICO_OVERCLOCK_HZ 250000
 #define GPIO_PIN_PWM 4
 
+uint slice;
 
 int main()
 {
@@ -17,9 +19,22 @@ int main()
 
   gpio_set_function(GPIO_PIN_PWM, GPIO_FUNC_PWM);
 
-  gpio_set_drive_strength(GPIO_PIN_PWM, 2);
+  gpio_set_drive_strength(GPIO_PIN_PWM, 3);
 
   gpio_set_slew_rate(GPIO_PIN_PWM, 1);
+
+  slice = pwm_gpio_to_slice_num(GPIO_PIN_PWM);
   
-  while(1);
+  pwm_set_wrap(slice_num, 5);
+  
+  pwm_set_clkdiv(slice_num, 1.0f);
+
+  pwm_set_chan_level(slice_num, PWM_CHAN_A, 3);
+
+  pwm_set_mask_enabled((1u << slice_num));
+   
+  while(1)
+  {
+
+  }
 }
