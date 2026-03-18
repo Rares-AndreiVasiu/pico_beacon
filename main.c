a9ce5f73f026edb542f0e1b9a6a92cd5017c6b02
Rares-AndreiVasiu <rares_vasiu03@yahoo.ro>
2026-03-18 08:28:01 +0200

    TO DO: check for pwm strength

--- main.c
+++ main.c
@@ -14,6 +14,12 @@ int main()
   set_sys_clock_khz(PICO_OVERCLOCK_HZ, true);
    
   stdio_init_all();
+
+  gpio_set_function(GPIO_PIN_PWM, GPIO_FUNC_PWM);
+
+  gpio_set_drive_strength(GPIO_PIN_PWM, 2);
+
+  gpio_set_slew_rate(GPIO_PIN_PWM, 1);
   
   while(1);
 }
